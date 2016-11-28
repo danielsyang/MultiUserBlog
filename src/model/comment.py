7989a58a7c9e8acab4f6b436bfbdf962006b4473
shuminyang <shu.minyang@hotmail.com>
2016-11-28 13:53:19 -0200

    add doc in comment.py

--- src/model/comment.py
+++ src/model/comment.py
@@ -4,6 +4,9 @@ from src.model.user import User
 
 
 class Comment(db.Model):
+    """
+    This class represents the 'Comment' entity. It references one post and has one owner.
+    """
     content = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
     post_id = db.ReferenceProperty(Post, required=True)
